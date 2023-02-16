56751055bf1cf10458481f1a2c7d69016a1e2019
Chzapps India <65512279+chzappsinc@users.noreply.github.com>
2023-02-16 17:11:20 +0530

    Update VideoWatermarkModule.java

--- android/src/main/java/com/vyoo/VideoWatermarkModule.java
+++ android/src/main/java/com/vyoo/VideoWatermarkModule.java
@@ -1,4 +1,4 @@
-package me.vyoo;
+package com.vyoo;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -18,84 +18,85 @@ import android.graphics.BitmapFactory;
 
 public class VideoWatermarkModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public VideoWatermarkModule(ReactApplicationContext reactContext) {
-      super(reactContext);
-      this.reactContext = reactContext;
-  }
+    public VideoWatermarkModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @Override
-  public String getName() {
-      return "VideoWatermark";
-  }
+    @Override
+    public String getName() {
+        return "VideoWatermark";
+    }
 
-  @ReactMethod
-  public void convert(String videoPath, String imagePath, String watermarkPosition, Callback callback) {
-      watermarkVideoWithImage(videoPath, imagePath, watermarkPosition, callback);
-  }
+    @ReactMethod
+    public void convert(String videoPath, String imagePath, String watermarkPosition, Callback callback) {
+        watermarkVideoWithImage(videoPath, imagePath, watermarkPosition, callback);
+    }
 
-  public void watermarkVideoWithImage(String videoPath, String imagePath, String watermarkPosition final Callback callback) {
-    File destFile = new File(this.getReactApplicationContext().getFilesDir(), "converted.mp4");
-      if (!destFile.exists()) {
-          try {
-              destFile.createNewFile();
-          } catch (IOException e) {
-              e.printStackTrace();
-          }
-      }
-      final String destinationPath = destFile.getPath();
-      private Position wtrkMrkPos;
-      switch(watermarkPosition) {
-        
-        case "LEFT_TOP":
-            wtrkMrkPos = GlWatermarkFilter.Position.LEFT_TOP;
-            break;
+    public void watermarkVideoWithImage(String videoPath, String imagePath, String watermarkPosition, final Callback callback Callback callback) {
+        File destFile = new File(this.getReactApplicationContext().getFilesDir(), "converted.mp4");
+        if (!destFile.exists()) {
+            try {
+                destFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        final String destinationPath = destFile.getPath();
+        GlWatermarkFilter.Position wtrkMrkPos;
+        switch (watermarkPosition) {
 
-        case "LEFT_BOTTOM":
-            wtrkMrkPos = GlWatermarkFilter.Position.LEFT_BOTTOM;
-            break;
-                  
-        case "RIGHT_TOP":
-            wtrkMrkPos = GlWatermarkFilter.Position.RIGHT_TOP;
-            break;
-        
-        case "RIGHT_BOTTOM":
-            wtrkMrkPos = GlWatermarkFilter.Position.RIGHT_BOTTOM;
-            break;
-        
-        default 
-            wtrkMrkPos = GlWatermarkFilter.Position.LEFT_TOP;
-            break;
-            
-      }
-      try {
-          new Mp4Composer(Uri.fromFile(new File(videoPath)), destinationPath, reactContext)
-                  .filter(new GlWatermarkFilter(BitmapFactory.decodeStream(reactContext.getContentResolver().openInputStream(Uri.fromFile(new File(imagePath))))), wtrkMrkPos)
-                  .listener(new Mp4Composer.Listener() {
-                      @Override
-                      public void onProgress(double progress) {
-                          Log.e("Progress", progress + "");
-                      }
-                      @Override
-                      public void onCompleted() {
-                          callback.invoke(destinationPath);
-                      }
+            case "LEFT_TOP":
+                wtrkMrkPos = GlWatermarkFilter.Position.LEFT_TOP;
+                break;
 
-                      @Override
-                      public void onCanceled() {
-                        
-                      }
+            case "LEFT_BOTTOM":
+                wtrkMrkPos = GlWatermarkFilter.Position.LEFT_BOTTOM;
+                break;
 
-                      @Override
-                      public void onFailed(Exception exception) {
-                          exception.printStackTrace();
-                      }
-                  }).start();
-      } catch (FileNotFoundException e) {
-          e.printStackTrace();
-      }
-  }
+            case "RIGHT_TOP":
+                wtrkMrkPos = GlWatermarkFilter.Position.RIGHT_TOP;
+                break;
+
+            case "RIGHT_BOTTOM":
+                wtrkMrkPos = GlWatermarkFilter.Position.RIGHT_BOTTOM;
+                break;
+
+            default:
+                wtrkMrkPos = GlWatermarkFilter.Position.LEFT_TOP;
+                break;
+
+        }
+        try {
+            new Mp4Composer(Uri.fromFile(new File(videoPath)), destinationPath, reactContext)
+                    .filter(new GlWatermarkFilter(BitmapFactory.decodeStream(reactContext.getContentResolver().openInputStream(Uri.fromFile(new File(imagePath))))), wtrkMrkPos)
+                    .listener(new Mp4Composer.Listener() {
+                        @Override
+                        public void onProgress(double progress) {
+                            Log.e("Progress", progress + "");
+                        }
+
+                        @Override
+                        public void onCompleted() {
+                            callback.invoke(destinationPath);
+                        }
+
+                        @Override
+                        public void onCanceled() {
+
+                        }
+
+                        @Override
+                        public void onFailed(Exception exception) {
+                            exception.printStackTrace();
+                        }
+                    }).start();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
